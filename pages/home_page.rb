35508388f60e13d6ed85e5f54988059a072cf71d
Ramesh Baskar <ramesh.baskarasubramanian@live.com>
2015-12-24 11:45:16 +0800

    Removed the search button and the corresponding action from the HomePage as its not required

--- pages/home_page.rb
+++ pages/home_page.rb
@@ -2,12 +2,10 @@ class HomePage < SitePrism::Page
   set_url "http://www.google.com.sg/"
 
   element :search_field, "input[name=q]"
-  element :search_button, "input[name=btnK]"
 
   def search_for(text)
     wait_for_search_field
     search_field.set text
-    # search_button.click
   end
 
   def searched?(text)
