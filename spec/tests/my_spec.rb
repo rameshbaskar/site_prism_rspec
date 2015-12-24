205b184c0a41f1aea0ae2f40e288174ea2566c17
Ramesh Baskar <ramesh.baskarasubramanian@live.com>
2015-12-24 11:44:39 +0800

    Renamed the spec file to end with _spec as required by rspec

--- spec/tests/my_spec.rb
+++ spec/tests/my_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../../spec/page_helper'
 
-describe "my spec 1" do
-  it "should search" do
+describe "my spec" do
+  it "should be able to search" do
     $home_page.load
     $home_page.search_for("Google")
     expect($home_page.searched?("Google")).to be_truthy
