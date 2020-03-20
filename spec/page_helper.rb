347e737554a16dad6d04973bb704a3982815741a
Ramesh Baskarasubramanian <ramesh.baskarasubramanian@live.com>
2020-03-20 11:58:48 +0530

    Changed the default browser to chrome instead of firefox

--- spec/page_helper.rb
+++ spec/page_helper.rb
@@ -1,27 +1,11 @@
 require_relative 'spec_helper'
+require_all 'pages'
 
-def firefox_profile
-  Selenium::WebDriver::Firefox::Profile.new
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
 end
 
-def firefox_options
-  options = {:browser => :firefox}
-  options[:profile] = firefox_profile
-  http_client = Selenium::WebDriver::Remote::Http::Default.new
-  http_client.timeout = 100
-  options[:http_client] = http_client
-  options
-end
-
-Capybara.register_driver :firefox do |app|
-  Capybara::Selenium::Driver.new(app, firefox_options)
-end
-
-Capybara.current_driver = :firefox
-Capybara.default_driver = :firefox
-
-Dir.glob(File.join(File.expand_path("../../pages", __FILE__), "**", "*.rb")).each do |page_class|
-  require page_class
-end
+Capybara.current_driver = :chrome
+Capybara.default_driver = :chrome
 
 $home_page = HomePage.new
